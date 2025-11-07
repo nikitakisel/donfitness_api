a73646a19181ca9516e08bc06e6a23e09524dae1
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2025-11-07 14:31:08 +0300

    add cascade deleting in models

--- models.py
+++ models.py
@@ -14,15 +14,15 @@ class User(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    resident = relationship("Resident", back_populates="user", uselist=False)
-    news = relationship("News", back_populates="user")
+    resident = relationship("Resident", back_populates="user", uselist=False, cascade="all, delete-orphan")
+    news = relationship("News", back_populates="user", cascade="all, delete-orphan")
 
 
 class News(Base):
     __tablename__ = "news"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     post_title = Column(String)
     post_info = Column(Text)
     post_image = Column(Text)
@@ -35,7 +35,7 @@ class Resident(Base):
     __tablename__ = "residents"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
     surname = Column(String)
     name = Column(String)
     birthdate = Column(DateTime)
@@ -43,7 +43,7 @@ class Resident(Base):
     phone = Column(String)
 
     user = relationship("User", back_populates="resident")
-    trainings = relationship("ResidentToTraining", back_populates="resident")
+    trainings = relationship("ResidentToTraining", back_populates="resident", cascade="all, delete-orphan")
 
 
 class Coach(Base):
@@ -56,30 +56,29 @@ class Coach(Base):
     qualification = Column(Text)
     extra_info = Column(Text)
 
-    training_sessions = relationship("TrainingSession", back_populates="coach")
+    training_sessions = relationship("TrainingSession", back_populates="coach", cascade="all, delete-orphan")
 
 
 class TrainingType(Base):
     __tablename__ = "training_types"
-
     id = Column(Integer, primary_key=True, index=True)
     training_name = Column(String)
     description = Column(Text)
 
-    training_sessions = relationship("TrainingSession", back_populates="training_type")
+    training_sessions = relationship("TrainingSession", back_populates="training_type", cascade="all, delete-orphan")
 
 
 class TrainingSession(Base):
     __tablename__ = "training_sessions"
 
     id = Column(Integer, primary_key=True, index=True)
-    training_type_id = Column(Integer, ForeignKey("training_types.id"), index=True)
-    coach_id = Column(Integer, ForeignKey("coaches.id"), index=True)
+    training_type_id = Column(Integer, ForeignKey("training_types.id", ondelete="CASCADE"), index=True)
+    coach_id = Column(Integer, ForeignKey("coaches.id", ondelete="CASCADE"), index=True)
     start_time = Column(DateTime)
     duration = Column(Integer)
     max_capacity = Column(Integer)
 
-    residents = relationship("ResidentToTraining", back_populates="training_session")
+    residents = relationship("ResidentToTraining", back_populates="training_session", cascade="all, delete-orphan")
     training_type = relationship("TrainingType", back_populates="training_sessions")
     coach = relationship("Coach", back_populates="training_sessions")
 
@@ -92,8 +91,8 @@ class ResidentToTraining(Base):
     __tablename__ = "residents_to_trainings"
 
     id = Column(Integer, primary_key=True, index=True)
-    resident_id = Column(Integer, ForeignKey("residents.id"), index=True)
-    training_session_id = Column(Integer, ForeignKey("training_sessions.id"), index=True)
+    resident_id = Column(Integer, ForeignKey("residents.id", ondelete="CASCADE"), index=True)
+    training_session_id = Column(Integer, ForeignKey("training_sessions.id", ondelete="CASCADE"), index=True)
 
     resident = relationship("Resident", back_populates="trainings")
     training_session = relationship("TrainingSession", back_populates="residents")
